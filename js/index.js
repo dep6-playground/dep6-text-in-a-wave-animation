077b5804d194c851bfeddb8f034d098f30048d0e
Uvin Weththasinghe <uvin6667@gmail.com>
2020-11-13 06:42:16 +0530

    3rd commit

--- js/index.js
+++ js/index.js
@@ -1,7 +1,31 @@
 document.getElementById('btn-show').onclick=function (){
     var inputText = document.getElementById('input-text').value;
     var stageElement = document.getElementById('stage');
-    stageElement.style.color='black';
-    stageElement.innerText=inputText;
+    console.log(inputText);
+    var radius = 250;
+    var angleIncrement = 2 * Math.PI / inputText.length;
+    var displacementIncrement = 1000/inputText.length;
+    var angle = 0;
+    var displacement=0;
+    var offset = 250;
+
+    for (var i = 0; i < inputText.length; i++) {
+        var spanElement = document.createElement('span');
+        spanElement.className = 'char';
+        spanElement.innerText = inputText.charAt(i);
+        stageElement.append(spanElement);
+
+        var y = radius * Math.sin(angle);
+        var x = displacement;
+
+        spanElement.style.left = x + "px";
+        spanElement.style.bottom = offset+ y + "px";
+
+        angle += angleIncrement;
+        displacement += displacementIncrement;
+    }
+
+
+
 
 }

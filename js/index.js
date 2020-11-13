c9354b057e7ad7a850200889ea489dc34a97d641
Uvin Weththasinghe <uvin6667@gmail.com>
2020-11-13 08:37:13 +0530

    Add the finalized files

--- js/index.js
+++ js/index.js
@@ -1,12 +1,26 @@
-document.getElementById('btn-show').onclick=function (){
+
+var timerId=-1;
+
+document.querySelector("#btn-start").addEventListener('click',function (){
+    timerId=setInterval(show,500);
+});
+
+document.querySelector("#btn-end").addEventListener('click',function (){
+    clearInterval(timerId);
+});
+
+var xd=0;
+var yd=0;
+function show(){
     var inputText = document.getElementById('input-text').value;
     var stageElement = document.getElementById('stage');
+    stageElement.innerText="";
     console.log(inputText);
     var radius = 250;
     var angleIncrement = 2 * Math.PI / inputText.length;
     var displacementIncrement = 1000/inputText.length;
     var angle = 0;
-    var displacement=0;
+    var displacement = 0;
     var offset = 250;
 
     for (var i = 0; i < inputText.length; i++) {
@@ -15,8 +29,8 @@ document.getElementById('btn-show').onclick=function (){
         spanElement.innerText = inputText.charAt(i);
         stageElement.append(spanElement);
 
-        var y = radius * Math.sin(angle);
-        var x = displacement;
+        var y =  radius * Math.sin(angle+yd);
+        var x =xd+ displacement;
 
         spanElement.style.left = x + "px";
         spanElement.style.bottom = offset+ y + "px";
@@ -24,8 +38,7 @@ document.getElementById('btn-show').onclick=function (){
         angle += angleIncrement;
         displacement += displacementIncrement;
     }
-
-
-
-
+    stageElement.style.color="black";
+    xd += displacementIncrement;
+    yd +=angleIncrement;
 }
